c1439be542e34eb11e2af6bd5b7bbdff945ba35a
Armine <https://github.com/ArmineKh>
2020-05-07 23:40:29 +0400

    day 3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
+/////////////////////////////////////////////////////////
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-////////////////////////////////////////
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -18,11 +11,43 @@ const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
-
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+const restaurantTitle = document.querySelector('.restaurant-title');
+const rating = document.querySelector('.rating');
+const minPrice = document.querySelector('.price');
+const category = document.querySelector('.category');
+const inputSearch = document.querySelector('.input-search');
 
 let login = localStorage.getItem('login');
 
+const getData = async function(url){
+  const response = await window.fetch(url);
+  if (!response.ok) {
+    throw new Error(`Error on url  ${url}, status of error  ${response.status}`);
+  } 
+  return await response.json();
+}
+
+
+
+const valid = function(str){
+  const nameReg = /^[a-zA-Z][a-zA-Z0-9-_\.]{1.20}$/;
+  return nameReg.test(str);
+}
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
 function toggleModalAuth(){
+  loginInput.style.borderColor = '';
+  loginInput.classList.remove('placeholder-red');
+  loginInput.placeholder = '';
   modalAuth.classList.toggle('is-open');
 }
 
@@ -37,6 +62,7 @@ function authorized(){
 
     buttonOut.removeEventListener('click', logOut);
     checkAuth();
+    returnMain();
   }
 
   userName.textContent = login;
@@ -47,29 +73,32 @@ function authorized(){
 
   buttonOut.addEventListener('click', logOut);
 }
+
+function maskInput(string){
+ return string.trim();
+}
+
 function notAuthorized(){
 
   function logIn(event){
     event.preventDefault();
-    login = loginInput.value;
-    localStorage.setItem('login', login);
+    if (maskInput(loginInput.value)) {
+    // if (valid(loginInput.value)) {
 
-
-    // toggleModalAuth();
-    // checkAuth();
-    if (login) {
+      login = loginInput.value;
+      localStorage.setItem('login', login);
       toggleModalAuth();
       buttonAuth.removeEventListener('click', toggleModalAuth);
       closeAuth.removeEventListener('click', toggleModalAuth);
       logInForm.removeEventListener('submit', logIn);
       logInForm.reset();
       checkAuth();
-    } else{
-      loginInput.style.border = '1px solid red';
+    } else {
+      loginInput.style.borderColor = 'red';
       loginInput.classList.add('placeholder-red');
       loginInput.placeholder = 'Enter your login';
-      
     }
+    
   }
   buttonAuth.addEventListener('click', toggleModalAuth);
   closeAuth.addEventListener('click', toggleModalAuth);
@@ -84,4 +113,174 @@ function checkAuth(){
     notAuthorized();
   }
 }
+
+ function createCardRestaurant(restaurant){
+  const { 
+          image, 
+          kitchen, 
+          name, 
+          price, 
+          stars, 
+          products,
+          time_of_delivery 
+          } = restaurant; 
+
+
+          const card = document.createElement('a');
+          card.className = 'card card-restaurant';
+          card.products = products;
+          card.info = [name, price, stars, kitchen];
+         card.insertAdjacentHTML('beforeend', `
+						<img src="${image}"/>
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title">${name}</h3>
+								<span class="card-tag tag">${time_of_delivery}</span>
+							</div>
+							<div class="card-info">
+								<div class="rating">
+									${stars}
+								</div>
+								<div class="price">${price} ₽</div>
+								<div class="category">${kitchen}</div>
+							</div>
+						</div>
+   `);
+   cardsRestaurants.insertAdjacentElement('beforeend', card);
+ }
+ 
+
+ function createCardGood({ description, id, image, name, price }){
+   
+   const card = document.createElement('div');
+   card.className = 'card';
+   card.insertAdjacentHTML('beforeend', `
+   <img src="${image}" alt="image" class="card-image"/>
+   <div class="card-text">
+     <div class="card-heading">
+       <h3 class="card-title card-title-reg">${name}</h3>
+     </div>
+     <div class="card-info">
+       <div class="ingredients">
+       ${description}
+       </div>
+     </div>
+     <div class="card-buttons">
+       <button class="button button-primary button-add-cart">
+         <span class="button-card-text">В корзину</span>
+         <span class="button-cart-svg"></span>
+       </button>
+       <strong class="card-price-bold">${price} ₽</strong>
+     </div>
+   </div>
+   `);
+   cardsMenu.insertAdjacentElement('beforeend', card);
+ }
+
+ function openGoods(event){
+   const target = event.target;
+   const restaurant = target.closest('.card-restaurant');
+  
+   if (restaurant) {
+    if(login){
+
+     const [name, price, stars, kitchen] = restaurant.info;
+
+     cardsMenu.textContent = '';
+     containerPromo.classList.add('hide');
+     restaurants.classList.add('hide');
+     menu.classList.remove('hide');
+
+     restaurantTitle.textContent = name;
+     rating.textContent = stars;
+     minPrice.textContent = `Oт ${price} ₽`;
+     category.textContent = kitchen;
+
+     getData(`./db/${restaurant.products}`).then(function(data){
+      data.forEach(createCardGood);
+    });
+  
+    }
+    else{
+      toggleModalAuth();
+    }
+   }
+   
+ }
+
+ function init(){
+
+getData('./db/partners.json').then(function(data){
+  data.forEach(createCardRestaurant);
+});
+
+
+
+ cartButton.addEventListener("click", toggleModal);
+
+ close.addEventListener("click", toggleModal);
+
+ cardsRestaurants.addEventListener('click', openGoods);
+ 
+
+ logo.addEventListener('click', function(){
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+  menu.classList.add('hide');
+ });
+
+ inputSearch.addEventListener('keydown', function(event){
+   if (event.keyCode === 13) {
+     const target = event.target;
+     const value = target.value.toLowerCase().trim();
+     if (value) {
+       target.style.borderColor = 'tomato';
+       setTimeout(function(){
+        target.style.backgroundColor = '';
+       }, 2000);
+     }
+     target.value = '';
+     const goods = [];
+     getData('./db/partners.json').then(function(data){
+       const products = data.map(function(item){
+         return item.products
+       });
+       products.forEach(function(product){
+        getData(`./db/${product}`)
+        .then(function(data){
+          goods.push(...data)
+          const searchGoods = goods.filter(function(item){
+            return item.name.toLowerCase().includes(value);
+          })
+          cardsMenu.textContent = '';
+     containerPromo.classList.add('hide');
+     restaurants.classList.add('hide');
+     menu.classList.remove('hide');
+
+     restaurantTitle.textContent = 'Result of search';
+     rating.textContent = '';
+     minPrice.textContent = '';
+     category.textContent = 'kitchen';
+          return searchGoods;
+    })
+    .then(function(data){
+      data.forEach(createCardGood)
+
+        })
+       })
+     });
+     
+   }
+})
+
+ new Swiper ('.swiper-container', {
+   loop: true,
+   autoplay: {
+     delay: 3000,
+   },
+ })
+
  checkAuth();
+ }
+
+ init();
